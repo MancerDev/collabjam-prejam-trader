1e1062f883ec3a19681f88a542f0952cd63711b7
jktulord <olden112002@mail.ru>
2025-03-29 03:04:36 +0300

    Merge

--- src/scenes/trade_scene/TimeControl.gd
+++ src/scenes/trade_scene/TimeControl.gd
@@ -9,17 +9,18 @@ func _ready():
 	
 
 func TimeUpdate(hour, customers, day = 0):
-	#SceneChangeSingleton
+	var SceneChangeSingleton = get_node("/root/SceneChangeSingleton") 
 	$Time.text = str(hour)+":00";
 	$Customers.text = "Customers:"+str(customers)
 	$Day.text = "Day:"+str(day)
 	var tween = create_tween()
 	if (hour > 20):
 		#$"../../Uninterractables/NightBackground".show()
-		#SceneChangeSingleton.music_next = SceneChangeSingleton.MusicSelection.Night
+		SceneChangeSingleton.music_next = SceneChangeSingleton.MusicSelection.Night
 		tween.tween_property($"../../Uninterractables/NightBackground", "modulate:a", 1.0, 1)
 		tween.tween_property($"../../Uninterractables/DayBackground", "modulate:a", 0.0, 1)
 	else:
+		SceneChangeSingleton.music_next = SceneChangeSingleton.MusicSelection.Day
 		tween.tween_property($"../../Uninterractables/DayBackground", "modulate:a", 1.0, 1)
 		tween.tween_property($"../../Uninterractables/NightBackground", "modulate:a", 0.0, 1)
 	pass
